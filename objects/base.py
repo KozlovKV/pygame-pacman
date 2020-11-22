5b284a96bc5184d8c21e0248601d6c6f40622b0c
Kibenko Daria <stegankaeasy@gmail.com>
2020-11-22 15:48:07 +0300

    Updated DrawableObject and ImageObject

--- objects/base.py
+++ objects/base.py
@@ -1,24 +1,35 @@
 import pygame
 
 
-class DrawableObject:     # TODO: bring here pygame.sprite.Sprite inheritance
-    def __init__(self, game) -> None:
+class DrawableObject:
+    def __init__(self, game, x = 0, y = 0, w = 0, h = 0, color):
         self.game = game
-        self.rect = pygame.rect.Rect(0, 0, 0, 0)
+        self.rect = pygame.rect.Rect(x, y, w, h)
+        self.color = color
+        self.alive = True
 
-    def move(self, x: int, y: int) -> None:
+    def collision(self, other):
+        return self.rect.colliderect(other.rect)
+
+    def die(self):
+        self.alive = False
+        self.rect.x = -1000
+        self.rect.y = -1000
+
+    def move(self, x, y):
         self.rect.x = x
         self.rect.y = y
 
-    def move_center(self, x: int, y: int) -> None:
+    def move_center(self, x, y):
         self.rect.centerx = x
         self.rect.centery = y
 
-    def process_event(self, event: pygame.event.Event) -> None:
+    def process_event(self, event):
         pass
 
-    def process_logic(self) -> None:
+    def process_logic(self):
         pass
 
-    def process_draw(self) -> None:
-        pass  # use self.game.screen for drawing, padawan
+    def process_draw(self):
+        if self.alive == True:
+            pygame.draw.rect(self.game.screen, self.rect, self.color)
