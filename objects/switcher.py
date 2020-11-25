e39a8c2404ea2abda08646d32d5cd4aaf161c3ed
Povolotskii Artem <artpovol@gmail.com>
2020-11-25 03:21:20 +0300

    Added ArraySwitch class and some tests to settings

--- objects/switcher.py
+++ objects/switcher.py
@@ -1,2 +1,71 @@
-class ArrowSwitcher:
-    pass
+import pygame
+
+from constants import Color, MAIN_FONT
+from objects import ButtonObject
+from third_party.button import Button
+
+
+class ArrowSwitcher(ButtonObject):
+    def __init__(self, game,
+                 x: int, y: int, width: int, height: int,
+                 color: pygame.color.Color = None,
+                 start_index: int = 0, *args) -> None:
+        self.arguments = args
+        self.args_index = start_index
+        self.all_rect = pygame.rect.Rect(x, y, width, height)
+        self.mid_rect = pygame.rect.Rect(x + width // 4, y, width // 2, height)
+        self.left_rect = pygame.rect.Rect(x, y, width // 4, height)
+        self.right_rect = pygame.rect.Rect(x + (width // 4 * 3), y, width // 4, height)
+        super().__init__(game, x + width // 4, y, width // 2, height, color, None, args[start_index])
+        self.left_button = Button(
+            rect=self.left_rect,
+            color=self.color,
+            function=self.dec_arg,
+            text="<",
+            **self.BUTTON_STYLE
+        )
+        self.right_button = Button(
+            rect=self.right_rect,
+            color=self.color,
+            function=self.inc_arg,
+            text=">",
+            **self.BUTTON_STYLE
+        )
+
+    def dec_arg(self) -> None:
+        self.args_index -= 1
+        if self.args_index < 0:
+            self.args_index = len(self.arguments)-1
+        self.set_text(self.arguments[self.args_index])
+
+    def inc_arg(self) -> None:
+        self.args_index += 1
+        if self.args_index > len(self.arguments)-1:
+            self.args_index = 0
+        self.set_text(self.arguments[self.args_index])
+
+    def set_center(self, x: int, y: int) -> None:
+        super(ButtonObject, self).set_center(x, y)
+        self.button.rect = self.rect
+        self.left_button.rect = self.rect
+        self.right_button.rect = self.rect
+
+    def set_position(self, x: int, y: int) -> None:
+        super().set_position(x, y)
+        self.button.rect = self.rect
+        self.left_button.rect = self.rect
+        self.right_button.rect = self.rect
+
+    def set_text(self, text) -> None:
+        self.text = text
+        self.button.text = text
+        self.button.render_text()
+
+    def process_event(self, event) -> None:
+        self.left_button.check_event(event)
+        self.right_button.check_event(event)
+
+    def process_draw(self) -> None:
+        self.button.update(self.game.screen)
+        self.left_button.update(self.game.screen)
+        self.right_button.update(self.game.screen)
--- scenes/settings.py
+++ scenes/settings.py
@@ -1,5 +1,6 @@
 from constants import Color
 from objects import ButtonObject
+from objects.switcher import ArrowSwitcher
 from scenes import BaseScene
 
 
@@ -7,3 +8,9 @@ class SettingsScene(BaseScene):
     def create_objects(self) -> None:
         self.objects.append(ButtonObject(self.game, 10, 600, 200, 40, Color.SOFT_RED,
                                          self.game.exit_game, 'EXIT'))
+        self.objects.append(ArrowSwitcher(self.game, 10, 10, 200, 50, Color.SOFT_RED, 0,
+                                          'Var1', 'Var2', 'Var3'))
+        self.objects.append(ArrowSwitcher(self.game, 10, 70, 200, 50, Color.GREEN, 1,
+                                          'Var1', 'Var2', 'Var3'))
+        self.objects.append(ArrowSwitcher(self.game, 10, 130, 200, 50, Color.BLUE, 2,
+                                          'Var1', 'Var2', 'Var3'))
