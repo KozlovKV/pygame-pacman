f9942b65b0c88cfb0403f7de5e065cd3228d0a07
Савин Сергей <sergeyrid@yandex.ru>
2020-11-27 01:07:04 +0300

    Implement process_level function for ghost class

--- objects/ghost.py
+++ objects/ghost.py
@@ -3,24 +3,67 @@ from queue import Queue
 from enum import Enum
 
 
-class Modes(Enum):
+WALL_SYMBOL = '#'
+
+
+class Status(Enum):
     CHASE = 0
     SCATTER = 1
     FRIGHTENED = 2
 
 
+def is_empty(cell: chr) -> bool:
+    return not (cell == WALL_SYMBOL or str.isnumeric(cell))
+
+
+def find_neighbours(matrix: list, cell_row: int, cell_column: int, width: int, height: int):
+    cell = matrix[cell_row][cell_column]
+    if not is_empty(cell):
+        return []
+    neighbours = []
+    if cell_row < height - 1 and is_empty(matrix[cell_row + 1][cell_column]):
+        neighbours.append((cell_row + 1) * width + cell_column)
+    if cell_row > 0 and is_empty(matrix[cell_row - 1][cell_column]):
+        neighbours.append((cell_row - 1) * width + cell_column)
+    if cell_column < width - 1 and is_empty(matrix[cell_row][cell_column + 1]):
+        neighbours.append(cell_row * width + (cell_column + 1))
+    if cell_column > 0 and is_empty(matrix[cell_row][cell_column - 1]):
+        neighbours.append(cell_row * width + (cell_column - 1))
+    return neighbours
+
+
 # Преобразовывает игровое поле в граф в формате списка смежности
-def process_field(field) -> list:
-    return field  # TODO
+def process_level(level_file_name) -> list:
+    with open(level_file_name, 'r') as level:
+        width = int(level.readline().split(': ')[1])
+        height = int(level.readline().split(': ')[1])
+        matrix = [line.split() for line in level.readlines()]
+    graph = [[] for _i in range(width * height)]
+    teleports = dict()  # сопоставляет номеру пару клеток, связанных телепортом
+    for cell_row in range(height):
+        for cell_column in range(width):
+            cell = matrix[cell_row][cell_column]
+            graph[cell_row * width + cell_column] = find_neighbours(matrix, cell_row, cell_column, width, height)
+            if str.isnumeric(cell):
+                teleports[int(cell)] = teleports.get(int(cell), list())
+                teleports[int(cell)].append((cell_row, cell_column))
+    for pair in teleports.values():
+        first_neighbours = find_neighbours(matrix, pair[0][0], pair[0][1], width, height)
+        first_cell = first_neighbours[0] if len(first_neighbours) > 0 else 0
+        second_neighbours = find_neighbours(matrix, pair[-1][0], pair[-1][1], width, height)
+        second_cell = second_neighbours[0] if len(first_neighbours) > 0 else 0
+        graph[first_cell].append(second_cell)  # FIXME изменить в зависимости от того, как работает телепорт
+        graph[second_cell].append(first_cell)  # FIXME
+    return graph
 
 
 def bfs(graph: list, start: int, end: int) -> list:
     vertex_number = len(graph)
     queue = Queue()
     queue.put(start)
-    used = [0 for i in range(vertex_number)]
-    distances = [0 for i in range(vertex_number)]
-    parents = [0 for i in range(vertex_number)]
+    used = [0 for _i in range(vertex_number)]
+    distances = [0 for _i in range(vertex_number)]
+    parents = [0 for _i in range(vertex_number)]
     used[start] = True
     parents[start] = -1
     if start == end:
@@ -52,13 +95,12 @@ def find_path(graph: list, start: int, end: int) -> list:  # Формат гра
 
 
 class Ghost(ImageObject):
-    mode = Modes.CHASE
+    status = Status.CHASE
 
-    def __init__(self, game, filename: str, x: int, y: int, field, pacman):
+    def __init__(self, game, filename: str, x: int, y: int, level: str):  # level - имя файла с уровнем
         super().__init__(game, filename, x, y)
-        self.game = game
-        self.field = process_field(field)
-        self.pacman = pacman
+        self.level = process_level(level)
+        self.pacman = game.pacman
 
     def process_logic(self):
         pass  # TODO
@@ -68,8 +110,8 @@ class Blinky(Ghost):
     """Целевой клеткой всегда является пакман, даже в режиме разбегания.
     Красного цвета."""
 
-    def __init__(self, game, filename: str, x: int, y: int, field, pacman):
-        super().__init__(game, filename, x, y, field, pacman)
+    def __init__(self, game, filename: str, x: int, y: int, level):
+        super().__init__(game, filename, x, y, level)
 
     def process_logic(self):
         super().process_logic()
@@ -80,8 +122,8 @@ class Pinky(Ghost):
     """Целевой клеткой является позиция на 4 клетки впереди пакмана.
     Розового цввета."""
 
-    def __init__(self, game, filename: str, x: int, y: int, field, pacman):
-        super().__init__(game, filename, x, y, field, pacman)
+    def __init__(self, game, filename: str, x: int, y: int, level):
+        super().__init__(game, filename, x, y, level)
 
     def process_logic(self):
         super().process_logic()
@@ -93,8 +135,8 @@ class Inky(Ghost):
     Начинает погоню только после того, как пакман съест 30 точек.
     Синего цвета."""
 
-    def __init__(self, game, filename: str, x: int, y: int, field, pacman, blinky):
-        super().__init__(game, filename, x, y, field, pacman)
+    def __init__(self, game, filename: str, x: int, y: int, level, blinky):
+        super().__init__(game, filename, x, y, level)
         self.blinky = blinky
 
     def process_logic(self):
@@ -107,8 +149,8 @@ class Clyde(Ghost):
     В остальное время находится в режиме разбегания.
     Начинает погоню только после того, как пакман съест 1/3 всех точек.
     Оранжевого цвета."""
-    def __init__(self, game, filename: str, x: int, y: int, field, pacman):
-        super().__init__(game, filename, x, y, field, pacman)
+    def __init__(self, game, filename: str, x: int, y: int, level):
+        super().__init__(game, filename, x, y, level)
 
     def process_logic(self):
         super().process_logic()
