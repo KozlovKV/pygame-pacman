0ee3a1ff892f6fa4cfce8cd1e437ca9932ba462a
Kozlov Kirill <kozlov.kirill.v.work@gmail.com>
2020-11-28 23:51:36 +0600

    Added methods of TeleportObject

--- objects/teleport.py
+++ objects/teleport.py
@@ -4,16 +4,22 @@ from objects import ImageObject
 class TeleportObject:
     def __init__(self, game, x1, y1, x2, y2):
         self.game = game
-        self.point1 = ImageObject(game, './resources/teleport/teleport_0.png',
-                                  x1, y1, 2,
-                                  './resources/teleport/teleport_[F].png')
-        self.point2 = ImageObject(game, './resources/teleport/teleport_0.png',
-                                  x2, y2, 2,
-                                  './resources/teleport/teleport_[F].png')
+        self.points = [
+            ImageObject(game, './resources/teleport/teleport_0.png',
+                        x1, y1, 2, './resources/teleport/teleport_[F].png'),
+            ImageObject(game, './resources/teleport/teleport_0.png',
+                        x2, y2, 2, './resources/teleport/teleport_[F].png'),
+        ]
         self.last_teleported_object = None
 
     def check_collisions_with_entries(self, obj: ImageObject):
-        pass
+        if self.last_teleported_object != obj:
+            if self.points[0].collision(obj):
+                self.teleport_object(1, obj)
+            elif self.points[1].collision(obj):
+                self.teleport_object(0, obj)
 
-    def collision_reaction(self, obj: ImageObject):
-        pass
+    def teleport_object(self, point_index, obj: ImageObject):
+        self.last_teleported_object = obj
+        obj.set_position(self.points[point_index].rect.x,
+                         self.points[point_index].rect.y)
